a9c0440bcd26e493a838231d9e8a909c155b976f
Sertaç Ersan <kodersan1@gmail.com>
2026-04-21 09:34:04 +0300

    Product List

---
+++ Proje_OOP/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OOP.ProjeContext;
+using System.Linq;
+
+namespace Proje_OOP.Controllers
+{
+    public class ProductController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var values = context.Products.ToList();
+            return View(values);
+        }
+    }
+}
