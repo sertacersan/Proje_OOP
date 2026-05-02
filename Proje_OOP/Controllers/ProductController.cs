acd6f5d4a5439e5d07c6327fb8e01f35ed4361d3
Sertaç Ersan <kodersan1@gmail.com>
2026-05-02 13:44:42 +0300

    Ürünün güncellenmesi

--- Proje_OOP/Controllers/ProductController.cs
+++ Proje_OOP/Controllers/ProductController.cs
@@ -36,5 +36,23 @@ namespace Proje_OOP.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = context.Products.Where(x => x.Id == id).FirstOrDefault();
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product p)
+        {
+            var value = context.Products.Where(x => x.Id == p.Id).FirstOrDefault();
+            value.Name = p.Name;
+            value.Price = p.Price;
+            value.Stock = p.Stock;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
