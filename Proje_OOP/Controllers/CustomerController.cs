61abde1b934f13f2bc89072161c8e117bcc23e03
Sertaç Ersan <kodersan1@gmail.com>
2026-05-05 15:26:33 +0300

    Müşteriye ait controllerların yazılması

---
+++ Proje_OOP/Controllers/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+using Proje_OOP.Entity;
+using Proje_OOP.ProjeContext;
+using System.Linq;
+
+namespace Proje_OOP.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var value = context.Customers.ToList();
+            return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCustomer(Customer p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCustomer(int id)
+        {
+            var value = context.Customers.Where(x => x.Id == id).FirstOrDefault();
+            context.Remove(value);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id)
+        {
+            var value = context.Customers.Where(x => x.Id == id).FirstOrDefault();
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer p)
+        {
+            var value = context.Customers.Where(x => x.Id == p.Id).FirstOrDefault();
+            value.City = p.City;
+            value.Name = p.Name;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
